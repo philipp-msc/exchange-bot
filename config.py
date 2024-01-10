97715274302338760208af37f875083deb60dc94
admin <admin@admins-MacBook-Air.local>
2024-01-10 19:17:33 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = "6945446318:AAFZIVclGuQi6qTYQtA0m8DeRPzYeDg_Gt8"
+
+keys = {
+    "евро": "EUR",
+    "доллар": "USD",
+    "рубль": "RUB",
+}
---
+++ extensions.py
@@ -0,0 +1,43 @@
+
+import requests
+import json
+from config import keys
+
+class ConvertionException(Exception):
+    pass
+
+
+class CryptoConverter:
+    @staticmethod
+    def get_price( quote: str, base: str, amount: str ):
+
+
+        if quote == base:
+            raise ConvertionException(f"Невозможно перевести одинаковые валюты {base}")
+    
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+        
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+    
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f"Не удалось обработать количество {amount}")
+        
+        r = requests.get(f'https://v6.exchangerate-api.com/v6/6bbd6322c9e44fa79978fdf6/pair/{quote_ticker}/{base_ticker}/{amount}')
+                         
+                         
+        total_base = json.loads(r.content)['conversion_result']#[keys[base]] #* float(amount)
+        
+
+        return total_base
+    
+
+    
+    
